8ab1c9246a7d55cf22f10dfcdb3dcd913ebdf01a
Arathy <arathyrajeesh2@gmail.com>
2025-11-28 10:26:59 +0530

    add anothe email

--- laundry_shop/laundry_shop/settings.py
+++ laundry_shop/laundry_shop/settings.py
@@ -136,8 +136,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'arathyrajeesh2@gmail.com'
-EMAIL_HOST_PASSWORD = 'kzyjkrzlmfwvqgnd'
+EMAIL_HOST_USER = 'brightandshine48@gmail.com'
+EMAIL_HOST_PASSWORD = 'suijwzvjpczfgajn '
 
 # Login URL for redirecting unauthenticated users
 LOGIN_URL = '/login/'
